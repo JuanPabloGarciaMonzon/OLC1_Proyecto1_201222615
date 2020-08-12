03295dab0b44818bd9b19f319762dc4332b731ec
JuanPabloGarciaMonzon <pablo2011euro@gmail.com>
2020-08-11 21:04:18 -0600

    Inicio de Proyecto 1

---
+++ TextLine.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+class TextLineNumbers(tk.Canvas):
+    def __init__(self, *args, **kwargs):
+        tk.Canvas.__init__(self, *args, **kwargs)
+        self.textwidget = None
+
+    def attach(self, text_widget):
+        self.textwidget = text_widget
+
+    def redraw(self, *args):
+        '''redraw line numbers'''
+        self.delete("all")
+
+        i = self.textwidget.index("@0,0")
+        while True :
+            dline= self.textwidget.dlineinfo(i)
+            if dline is None: break
+            y = dline[1]
+            linenum = str(i).split(".")[0]
+            self.create_text(2,y,anchor="nw", text=linenum)
+            i = self.textwidget.index("%s+1line" % i)
